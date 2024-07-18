23e608bccc95011b32de6ef25e9706feef8aa54e
facundo <facga20145@gmail.com>
2024-07-17 19:49:16 -0500

    funciona todo

--- Vista/V_V_Excel/excel.php
+++ Vista/V_V_Excel/excel.php
@@ -26,6 +26,7 @@
 
         $horas = ["08:00-09:00", "09:00-10:00", "10:00-11:00", "11:00-12:00", "12:00-13:00", "13:00-14:00", "14:00-15:00", "15:00-16:00", "16:00-17:00", "17:00-18:00", "18:00-19:00", "19:00-20:00", "20:00-21:00", "21:00-22:00"];
         $dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+        $ocupadas = [];
 
         foreach ($asignaciones as $aula => $data) {
             echo "<h2>Aula: $aula</h2>";
@@ -41,6 +42,9 @@
                 echo "<tr><td>$hora_inicio-$hora_fin</td>";
 
                 foreach ($dias as $dia) {
+                    if (isset($ocupadas[$aula][$dia][$hora_inicio])) {
+                        continue;
+                    }
                     $celda_vacia = true;
 
                     if (isset($data[$dia])) {
@@ -52,6 +56,12 @@
                                 $rowspan = ceil($duracion_horas);
                                 $color = ($asignacion['escuela'] == 'EPIS') ? 'background-color: #d9ead3;' : 'background-color: #c9daf8;';
                                 echo "<td style='$color' rowspan='$rowspan'>{$asignacion['detalle']}</td>";
+
+                                for ($i = 0; $i < $rowspan; $i++) {
+                                    $hora_ocupada = date("H:i", strtotime("+$i hour", $hora_inicio_asignacion));
+                                    $ocupadas[$aula][$dia][$hora_ocupada] = true;
+                                }
+                                
                                 $celda_vacia = false;
                                 break;
                             }
